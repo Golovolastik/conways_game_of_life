e32f2ab8ef65707a822183a1cb5686b71ab03740
Golovolastik <yankovich.aliaksei@gmail.com>
2023-04-24 19:08:36 +0300

    feature: change screen color with space bar

--- main.c
+++ main.c
@@ -1,23 +1,13 @@
 #include <SDL.h>
 
-int run_black_screen();
+int run_black_screen(SDL_Window* window);
+int run_white_screen(SDL_Window* window);
 
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Ошибка инициализации SDL: %s\n", SDL_GetError());
         return 1;
     }
-    run_black_screen();
-
-    return 0;
-}
-
-int run_black_screen(){
-//    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-//        printf("Ошибка инициализации SDL: %s\n", SDL_GetError());
-//        return 1;
-//    }
-
     SDL_Window* window = SDL_CreateWindow("Мое окно",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
@@ -26,6 +16,60 @@ int run_black_screen(){
         printf("Ошибка при создании окна: %s\n", SDL_GetError());
         return 1;
     }
+    run_black_screen(window);
+    //run_white_screen(window);
+
+    return 0;
+}
+
+int run_black_screen(SDL_Window* window){
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Ошибка при создании рендерера: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Event event;
+    int done = 0;
+
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    done = 1;
+                    break;
+                }
+                case SDL_KEYDOWN: {
+                    if (event.key.keysym.sym == SDLK_SPACE) {
+                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                        SDL_RenderClear(renderer);
+
+                        SDL_RenderPresent(renderer);
+                    }
+                    break;
+                }
+                default: {break;}
+            }
+        }
+
+//        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+//        SDL_RenderClear(renderer);
+//
+//        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int run_white_screen(SDL_Window* window){
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL) {
@@ -43,14 +87,17 @@ int run_black_screen(){
                     done = 1;
                     break;
                 }
-                case SDLK_SPACE: {
+                case SDL_KEYDOWN: {
+                    if (event.key.keysym.sym == SDLK_SPACE) {
+                        run_white_screen(window);
+                    }
                     break;
                 }
                 default: {break;}
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
         SDL_RenderPresent(renderer);
@@ -60,3 +107,4 @@ int run_black_screen(){
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
+
