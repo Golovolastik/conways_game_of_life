b5d7fe12c92701d51a9b1ac8e5fc5e863d65d953
Golovolastik <yankovich.aliaksei@gmail.com>
2023-04-25 16:13:26 +0300

    feature: draw board

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <SDL.h>
+#include <stdbool.h>
 
+struct Board init_board(int height, int width);
+void draw_board(struct Board* board, SDL_Window* window, SDL_Renderer* renderer);
 int run_black_screen(SDL_Window*, SDL_Renderer*);
 int change_screen_color(SDL_Window*, SDL_Renderer*);
+
+struct Cell{
+    // true - white, false - black
+    bool color;
+};
+
+struct Board {
+    int height;
+    int width;
+    struct Cell** board_array;
+
+};
 
 
 
@@ -25,8 +40,40 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // calculate board
+    // render
+
+    // создание поля
+    struct Board board = init_board(10, 10);
+
+    draw_board(&board, window, renderer);
     //run_black_screen(window, renderer);
-    change_screen_color(window, renderer);
+    //change_screen_color(window, renderer);
+
+    // основной цикл обработки событий
+    SDL_Event event;
+    int quit = 0;
+
+    while (!quit) {
+        //draw_board(&board, window, renderer);
+        // обработка событий
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = 1;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_SPACE) {
+                        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+                        SDL_RenderClear(renderer);
+                        SDL_RenderPresent(renderer);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     return 0;
 }
@@ -42,6 +89,7 @@ int run_black_screen(SDL_Window* window, SDL_Renderer* renderer){
     int done = 0;
 
     while (!done) {
+
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT: {
@@ -113,4 +161,53 @@ int change_screen_color(SDL_Window* window, SDL_Renderer* renderer){
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+struct Board init_board(int height, int width){
+    struct Board board;
+    board.height = height;
+    board.width = width;
+    board.board_array = malloc(board.width *  sizeof(struct Cell*));
+    for (int i = 0; i < board.height; i++) {
+        board.board_array[i] = malloc(board.width * sizeof(struct Cell));
+        // Инициализируем каждый элемент структурой Cell
+        for (int j = 0; j < board.width; j++) {
+            struct Cell new_cell = { .color = true }; // Создаем новый экземпляр структуры Cell
+            board.board_array[i][j] = new_cell; // Присваиваем значение элементу массива
+        }
+    }
+
+    return board;
+}
+
+void draw_board(struct Board* board, SDL_Window* window, SDL_Renderer* renderer){
+    // отрисовка поля квадратов
+    SDL_Rect rects[100];
+    for (int i = 0; i < 100; i++) {
+        rects[i].x = (i % 10) * 50;
+        rects[i].y = (i / 10) * 50;
+        rects[i].w = 50;
+        rects[i].h = 50;
+    }
+
+    // заполняем экран белым цветом
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+
+    // отрисовываем 100 квадратов
+    for (int i = 0; i < 100; i++) {
+        // рисуем границу квадрата
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &rects[i]);
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    // показать рендер
+    //SDL_RenderPresent(renderer);
+    SDL_RenderFillRect(renderer, &rects[34]);
+    SDL_RenderPresent(renderer);
+}
+
+void render(SDL_Window* window, SDL_Renderer* renderer, struct Board* board){
+
 }
