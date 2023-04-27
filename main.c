4b54783eaa71ffe8b52457a0734ac98bb567df49
Golovolastik <yankovich.aliaksei@gmail.com>
2023-04-28 00:36:46 +0300

    feature: difene cell size and resolution, delete comments

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <SDL.h>
 #include <stdbool.h>
+#define SIZE 45
+#define CELL_SIZE 15
 
 struct Cell{
     // true - white, false - black
@@ -18,11 +20,9 @@ struct Board {
 
 int neighbors_count(struct Board* board, struct Cell* cell, int size);
 struct Board init_board(int height, int width);
-void draw_board(struct Board* board, SDL_Window* window, SDL_Renderer* renderer);
+void draw_board(struct Board* board, SDL_Renderer* renderer);
 void draw_cells(SDL_Rect rects[], SDL_Renderer* renderer, int size, struct Board* board);
 void recalculate(struct Board* board, int size);
-int run_black_screen(SDL_Window*, SDL_Renderer*);
-int change_screen_color(SDL_Window*, SDL_Renderer*);
 struct Cell check_rules(struct Cell* cell, int count);
 
 
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("Мое окно",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                              750, 750, SDL_WINDOW_SHOWN);
+                                              SIZE*CELL_SIZE, SIZE*CELL_SIZE, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         printf("Ошибка при создании окна: %s\n", SDL_GetError());
         return 1;
@@ -47,11 +47,11 @@ int main(int argc, char* argv[]) {
     }
 
     // создание поля
-    struct Board board = init_board(15, 15);
+    struct Board board = init_board(SIZE, SIZE);
 
     // стартовая раскладка клеток
-    // 2 квадрата
 
+    // 2 квадрата
     board.board_array[5][5].alive = true;
     board.board_array[5][6].alive = true;
     board.board_array[6][5].alive = true;
@@ -62,10 +62,29 @@ int main(int argc, char* argv[]) {
     board.board_array[4][4].alive = true;
 
     // палочка
+//    board.board_array[12][10].alive = true;
+//    board.board_array[12][11].alive = true;
+//    board.board_array[12][9].alive = true;
 
-    board.board_array[12][10].alive = true;
-    board.board_array[12][11].alive = true;
-    board.board_array[12][9].alive = true;
+    // пентадекатлон
+    board.board_array[17][10].alive = true;
+    board.board_array[17][11].alive = true;
+
+    board.board_array[16][12].alive = true;
+    //board.board_array[17][12].alive = true;
+    board.board_array[18][12].alive = true;
+
+    board.board_array[17][13].alive = true;
+    board.board_array[17][14].alive = true;
+    board.board_array[17][15].alive = true;
+    board.board_array[17][16].alive = true;
+
+    board.board_array[16][17].alive = true;
+    //board.board_array[17][17].alive = true;
+    board.board_array[18][17].alive = true;
+
+    board.board_array[17][18].alive = true;
+    board.board_array[17][19].alive = true;
 
     // хлам
 //    board.board_array[0][0].alive = true;
@@ -77,30 +96,13 @@ int main(int argc, char* argv[]) {
 //    board.board_array[8][0].alive = true;
 //    board.board_array[8][14].alive = true;
 //    board.board_array[11][12].alive = true;
-//    board.board_array[13][8].died = true;
-//    board.board_array[12][7].died = true;
-//    board.board_array[13][6].died = true;
-    //printf("x: %d y: %d", board.board_array[14][9].pos_y, board.board_array[14][9].pos_x);
-    //board.board_array[5][6].color = false;
-
-    // calculate neighbors
-//    for (int i=0; i<15; i++){
-//        for(int j=0; j<15; j++) {
-//            if (board.board_array[i][j].alive){
-//                printf("Cell [%d][%d] has %d neighbors.\n", j+1, i+1, neighbors_count(&board, &board.board_array[i][j], 15));
-//            }
-//        }
-//    }
-
-    //draw_board(&board, window, renderer);
 
     // основной цикл обработки событий
     SDL_Event event;
     int quit = 0;
     while (!quit) {
-        draw_board(&board, window, renderer);
-        recalculate(&board, 15);
-
+        draw_board(&board, renderer);
+        recalculate(&board, SIZE);
         // обработка событий
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -109,11 +111,8 @@ int main(int argc, char* argv[]) {
                     break;
                 case SDL_KEYDOWN:
                     if (event.key.keysym.sym == SDLK_SPACE) {
-                        recalculate(&board, 15);
-                        draw_board(&board, window, renderer);
-//                        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-//                        SDL_RenderClear(renderer);
-//                        SDL_RenderPresent(renderer);
+                        recalculate(&board, SIZE);
+                        draw_board(&board, renderer);
                     }
                     break;
                 default:
@@ -127,93 +126,6 @@ int main(int argc, char* argv[]) {
     SDL_Quit();
 
     return 0;
-}
-
-
-
-int run_black_screen(SDL_Window* window, SDL_Renderer* renderer){
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-
-    SDL_Event event;
-    int done = 0;
-
-    while (!done) {
-
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT: {
-                    done = 1;
-                    break;
-                }
-                case SDL_KEYDOWN: {
-                    if (event.key.keysym.sym == SDLK_SPACE) {
-                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-                        SDL_RenderClear(renderer);
-
-                        SDL_RenderPresent(renderer);
-                    }
-                    break;
-                }
-                default: {break;}
-            }
-        }
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-int change_screen_color(SDL_Window* window, SDL_Renderer* renderer){
-
-    int r, g, b;
-    r = g = b = 255;
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-
-    SDL_Event event;
-    int done = 0;
-
-    while (!done) {
-        r -= 5;
-        g -= 25;
-        b -= 15;
-        SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(200);
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT: {
-                    done = 1;
-                    break;
-                }
-                case SDL_KEYDOWN: {
-                    if (event.key.keysym.sym == SDLK_SPACE) {
-//                        r -= 15;
-//                        g -= 15;
-//                        b -= 15;
-//                        SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-//                        SDL_RenderClear(renderer);
-//
-//                        SDL_RenderPresent(renderer);
-                    }
-                    break;
-                }
-                default: {break;}
-            }
-        }
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 }
 
 struct Board init_board(int height, int width){
@@ -234,21 +146,15 @@ struct Board init_board(int height, int width){
     return board;
 }
 
-// works
-void draw_board(struct Board* board, SDL_Window* window, SDL_Renderer* renderer){
+void draw_board(struct Board* board, SDL_Renderer* renderer){
     // отрисовка поля квадратов
-    int size = 15;
-    SDL_Rect rects[size*size];
-
+    SDL_Rect rects[SIZE*SIZE];
     // заполняем экран белым цветом
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
-
-    draw_cells(rects, renderer, size, board);
-
+    draw_cells(rects, renderer, SIZE, board);
     // показать рендер
     SDL_RenderPresent(renderer);
-
     SDL_Delay(500);
 }
 
@@ -257,18 +163,16 @@ void draw_cells(SDL_Rect rects[], SDL_Renderer* renderer, int size, struct Board
     for (int i = 0; i<size; i++) {
         for (int j=0; j<size; j++) {
             // инициализируем клетки
-            rects[i*size + j].y = (i) * 50;
-            rects[i*size + j].x = (j) * 50;
-            rects[i*size + j].w = 50;
-            rects[i*size + j].h = 50;
+            rects[i*size + j].y = (i) * CELL_SIZE;
+            rects[i*size + j].x = (j) * CELL_SIZE;
+            rects[i*size + j].w = CELL_SIZE;
+            rects[i*size + j].h = CELL_SIZE;
             // закрашиваем клетки
             if (board->board_array[i][j].alive) {
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
                 SDL_RenderFillRect(renderer, &rects[i*size + j]);
             }
             // рисуем границу клеток
-//            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-//            SDL_RenderDrawRect(renderer, &rects[i * size + j]);
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderDrawRect(renderer, &rects[i * size + j]);
 
@@ -304,11 +208,10 @@ void recalculate(struct Board* board, int size) {
         temp_board[i] = malloc(size * sizeof(struct Cell));
         // Инициализируем каждый элемент структурой Cell
         for (int j = 0; j < size; j++) {
-            struct Cell new_cell = {
-                    .alive = false, /*.died = board->board_array[i][j].died,*/ .pos_x = j, .pos_y = i};
             // Создаем новый экземпляр структуры Cell
-            int neighbors = neighbors_count(board, &board->board_array[i][j], 15);
-            temp_board[i][j] = check_rules(&board->board_array[i][j], neighbors); // Присваиваем значение элементу массива
+            int neighbors = neighbors_count(board, &board->board_array[i][j], SIZE);
+            // Присваиваем значение элементу массива
+            temp_board[i][j] = check_rules(&board->board_array[i][j], neighbors);
             temp_board[i][j].pos_x = j;
             temp_board[i][j].pos_y = i;
         }
@@ -329,7 +232,6 @@ struct Cell check_rules(struct Cell* cell, int count){
     }
     if (count < 2) {
         result.alive = false;
-        //temp_board[i][j].died = true;
         // правило 2
     } else if (count > 3){
         result.alive = false;
