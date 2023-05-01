a6c37053e4c61f4a76cdc8e580ef3485d3cabda1
Golovolastik <yankovich.aliaksei@gmail.com>
2023-05-01 13:43:06 +0300

    fix: green-black theme

--- main.c
+++ main.c
@@ -172,7 +172,7 @@ void draw_board(struct Board* board, SDL_Renderer* renderer){
     // отрисовка поля квадратов
     SDL_Rect rects[SIZE*SIZE];
     // заполняем экран белым цветом
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 153, 51, 255);
     SDL_RenderClear(renderer);
     draw_cells(rects, renderer, SIZE, board);
     // показать рендер
@@ -191,7 +191,7 @@ void draw_cells(SDL_Rect rects[], SDL_Renderer* renderer, int size, struct Board
             rects[i*size + j].h = CELL_SIZE;
             // закрашиваем клетки
             if (board->board_array[i][j].alive) {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_SetRenderDrawColor(renderer, 51, 51, 51, 255);
                 SDL_RenderFillRect(renderer, &rects[i*size + j]);
             }
             // рисуем границу клеток
