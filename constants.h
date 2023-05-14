b47d1388bc4d6a9b108f35526316f30ea669281d
Golovolastik <yankovich.aliaksei@gmail.com>
2023-05-14 09:37:35 +0300

    fix: refactor

---
+++ constants.h
@@ -0,0 +1,12 @@
+//
+// Created by Алексей Янкович on 14.05.23.
+//
+
+#ifndef CONWAYS_GAME_OF_LIFE_CONSTANTS_H
+#define CONWAYS_GAME_OF_LIFE_CONSTANTS_H
+
+#define HEIGHT_SIZE 30
+#define WIDTH_SIZE 40
+#define CELL_SIZE 20
+
+#endif //CONWAYS_GAME_OF_LIFE_CONSTANTS_H
